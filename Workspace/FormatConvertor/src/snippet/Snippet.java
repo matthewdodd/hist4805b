4f141f6f6fc8ebe8c737822acdda6cfe262100f3
Matthew Dodd <matthew.a.dodd@gmail.com>
2015-02-01 13:52:34 -0500

    Copy of Eclipse and the workspace for the software in developments. For working anywhere.

---
+++ Workspace/FormatConvertor/src/snippet/Snippet.java
@@ -0,0 +1,10 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		import java.io.IOException;
+		import java.io.FileReader;
+		import java.io.BufferedReader;
+	}
+}
+
